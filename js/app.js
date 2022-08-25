ebf2e385355e353c44ca5a70ce3c022d46d09d19
devdev <51432531+devintaule@users.noreply.github.com>
2022-08-26 00:31:38 +0800

    commit portfolio css

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 const filter_btns = document.querySelectorAll(".filter-btn");
 const skills_wrap = document.querySelector(".skills");
 const skills_bars = document.querySelectorAll(".skill-progress");

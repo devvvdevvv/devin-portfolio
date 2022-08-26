ec6c41a6648f4d4ade9b3d9c6a37a434378c9d06
devdev <51432531+devintaule@users.noreply.github.com>
2022-08-26 11:31:12 +0800

    commit

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ const footer_input = document.querySelector(".footer-input");
 const hamburger_menu = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector("header nav");
 const links = document.querySelectorAll(".links a");
+const viewer = document.querySelectorAll(".plus");
 
 footer_input.addEventListener("focus", () => {
   footer_input.classList.add("focus");
